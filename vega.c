b22ebf949ee053ec3053b427ee7b7faa9c08958b
Frank Tao <48634762+kugelblitz2@users.noreply.github.com>
2021-03-30 18:09:06 -0400

    Update vega.c

--- vega.c
+++ vega.c
@@ -15,6 +15,8 @@ char *get_hostname(){
 
 char *get_osname();
 
+char *get_hwname();
+
 char *get_kernel(){
     struct utsname buffer;
     uname(&buffer);
@@ -23,3 +25,42 @@ char *get_kernel(){
     asprintf(&kernelname, "%s %s", buffer.sysname, buffer.version);
     return kernelname;
 }
+
+char *get_uptime();
+
+char *get_packages();
+
+char *get_shell();
+
+char *get_screenres();
+
+char *get_de();
+
+char *get_wm();
+
+char *get_terminal();
+
+char *get_termfont();
+
+char *get_cpu();
+
+char *get_gpu();
+
+char *get_vram();
+
+char *get_ram();
+
+char *get_gpudriver();
+
+char *get_df();
+
+char *get_battery();
+
+char *get_song();
+
+char *get_localip();
+
+char *get_ipv4();
+
+char *get_ipv6();
+
