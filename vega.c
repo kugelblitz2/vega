67adb8e92e8ba80daa5d467e7cd8b6e21083ad02
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2021-03-31 04:21:09 -0400

    Update vega.c

--- vega.c
+++ vega.c
@@ -3,14 +3,21 @@
 #include <stdio.h>
 #include "vega.h"
 
+#ifdef __linux__
+#include <sys/sysinfo.h>
+#endif
+#ifdef __APPLE__
+#include <sys/sysctl.h>
+#endif
+
 char *get_username(){
     return getenv("USER");
 }
 
 char *get_hostname(){
-    struct utsname buffer;
-    uname(&buffer);
-    return buffer.nodename;
+    struct utsname basicinfo;
+    uname(&basicinfo);
+    return basicinfo.nodename;
 }
 
 char *get_osname();
@@ -18,15 +25,27 @@ char *get_osname();
 char *get_hwname();
 
 char *get_kernel(){
-    struct utsname buffer;
-    uname(&buffer);
+    struct utsname basicinfo;
+    uname(&basicinfo);
     
     char *kernelname;
-    asprintf(&kernelname, "%s %s", buffer.sysname, buffer.version);
+    asprintf(&kernelname, "%s %s", basicinfo.sysname, basicinfo.version);
     return kernelname;
 }
 
-char *get_uptime();
+long *get_uptime(){
+
+    #ifdef __linux__
+        struct sysinfo linuxinfo;
+        sysinfo(&linuxinfo);
+
+        return linuxinfo.uptime;
+    #elif defined(__APPLE__)
+        struct timespec uptime;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &uptime);
+        return uptime.tv_sec;
+    #endif
+}
 
 char *get_packages();
 
@@ -48,7 +67,9 @@ char *get_gpu();
 
 char *get_vram();
 
-char *get_ram();
+char *get_ramused();
+
+char *get_ramtotal();
 
 char *get_gpudriver();
 
